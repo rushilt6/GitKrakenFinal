5fabbca252f73ebde4a2ff887dbe8a76e38bc64d
rushilt6 <rushil.thummaluru@gmail.com>
2022-03-16 21:15:49 -0400

    Added Comments PartnerA(Rushil)

--- Runner.java
+++ Runner.java
@@ -1,6 +1,9 @@
 
 public class Runner{
 	public Runner(){
+		//Rushil: I learned about lamda expressions and how they can shorten code length
+		//Rushil: I learned about git and how you can edit with other people
+		//Rushil: I learned about how you can create repositories with other people and work on projects together by using branches and then merging them into the main
 		printQuotient pq = (num1,num2) -> {
 			System.out.printf("%.3f%n",num1/num2);
 		};
